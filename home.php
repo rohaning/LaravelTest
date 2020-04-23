2233d72923ce384472eb475cf90498bdb995c4fe
rohaning <55027343+rohaning@users.noreply.github.com>
2020-04-23 22:51:54 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,10 +1,12 @@
 <?php
 
-echo 'Hi my name is Rohaning';
+echo 'Hi my name is Raj';
 
 eho '<br/>';
 
-echo 'I am a developer';
+echo 'I am a developer of laravel';
+
+echo 'Check test';
 
 echo 'Edited default branch';
 
@@ -12,6 +14,12 @@ echo '<br/>';
 
 echo 'Added new Branch';
 
+echo 'checking conflicts';
+
 echo 'Good Bye';
+
+$a = ['w','gosda','g'];
+
+print_r($a);
 
 ?>
